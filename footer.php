64c9f576d305e62c0277d2d74a459f680e104cd7
pebutler3 <p.e.butler3@gmail.com>
2016-10-06 01:24:46 -0600

    added footer styles

--- footer.php
+++ footer.php
@@ -1,32 +1,40 @@
-			
-	</div>
-			<!-- footer -->
-			<footer class="footer" role="contentinfo">
 
-				<!-- copyright -->
-				<p class="copyright">
-					&copy; <?php echo date('Y'); ?> Copyright <?php bloginfo('name'); ?>. <!-- <?php _e('Powered by', 'html5blank'); ?>
-					<a href="//wordpress.org" title="WordPress">WordPress</a> &amp; <a href="//html5blank.com" title="HTML5 Blank">HTML5 Blank</a>. -->
-				</p>
-				<!-- /copyright -->
-<section>Social Media</section>
-			</footer>
-			<!-- /footer -->
+  </div>
+  <!-- footer -->
+  <footer class="footer" role="contentinfo">
 
-	
-		<!-- /wrapper -->
+  <!-- copyright -->
+    <p class="copyright">
+      &copy; <?php echo date('Y'); ?> Copyright <?php bloginfo('name'); ?>. <!-- <?php _e('Powered by', 'html5blank'); ?>
+      <a href="//wordpress.org" title="WordPress">WordPress</a> &amp; <a href="//html5blank.com" title="HTML5 Blank">HTML5 Blank</a>. -->
+    </p>
+  <!-- /copyright -->
 
-		<?php wp_footer(); ?>
+    <section>
+        <a href="#" class="fa fa-facebook"></a>
+        <a href="#" class="fa fa-twitter"></a>
+        <a href="#" class="fa fa-linkedin"></a>
+        <a href="#" class="fa fa-google-plus"></a>
+    </section>
 
-		<!-- analytics -->
-		<script>
-		(function(f,i,r,e,s,h,l){i['GoogleAnalyticsObject']=s;f[s]=f[s]||function(){
-		(f[s].q=f[s].q||[]).push(arguments)},f[s].l=1*new Date();h=i.createElement(r),
-		l=i.getElementsByTagName(r)[0];h.async=1;h.src=e;l.parentNode.insertBefore(h,l)
-		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-		ga('create', 'UA-XXXXXXXX-XX', 'yourdomain.com');
-		ga('send', 'pageview');
-		</script>
+    <button class="back-to-top"><i class="fa fa-chevron-up"></i></button>
 
-	</body>
+  </footer>
+  <!-- /footer -->
+
+  <!-- /wrapper -->
+
+  <?php wp_footer(); ?>
+
+  <!-- analytics -->
+  <script>
+    (function(f,i,r,e,s,h,l){i['GoogleAnalyticsObject']=s;f[s]=f[s]||function(){
+    (f[s].q=f[s].q||[]).push(arguments)},f[s].l=1*new Date();h=i.createElement(r),
+    l=i.getElementsByTagName(r)[0];h.async=1;h.src=e;l.parentNode.insertBefore(h,l)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+    ga('create', 'UA-XXXXXXXX-XX', 'yourdomain.com');
+    ga('send', 'pageview');
+  </script>
+
+  </body>
 </html>
