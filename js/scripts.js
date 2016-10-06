ccc15312d6c62fabaf6fd6d0ab77474d1fdf38a1
pebutler3 <p.e.butler3@gmail.com>
2016-10-06 01:24:28 -0600

    added js for back-to-top button

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,32 @@
 (function ($, root, undefined) {
-	
-	$(function () {
-		
-		'use strict';
-		
-		// DOM ready, take it away
-		
-	});
-	
+
+  $(function () {
+
+    'use strict';
+
+     function runScroll() {
+       scrollTo(document.body, 0, 600);
+     }
+
+     var scrollme;
+     scrollme = document.querySelector(".back-to-top");
+     scrollme.addEventListener("click",runScroll,false)
+
+     function scrollTo(element, to, duration) {
+
+       if (duration <= 0) return;
+
+       var difference = to - element.scrollTop;
+       var perTick = difference / duration * 10;
+
+       setTimeout(function() {
+         element.scrollTop = element.scrollTop + perTick;
+         if (element.scrollTop == to) return;
+         scrollTo(element, to, duration - 10);
+       }, 10);
+
+     }
+
+  });
+
 })(jQuery, this);
